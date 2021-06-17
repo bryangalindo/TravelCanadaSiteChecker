6388535f6b8fcc069c1f9318fb8ceff1230ebf29
Bryan Galindo <galindo.bryan08@gmail.com>
2021-06-17 00:44:30 -0500

    Removing string literal to improve code maintainability

--- sms.py
+++ sms.py
@@ -2,10 +2,10 @@ from twilio.rest import Client
 import constants as c
 
 
-def send_sms_message(recipient_phone_number, twilio_phone_number):
+def send_sms_message(recipient_phone_number, twilio_phone_number, body):
     client = Client(c.TWILIO_ACCOUNT_SID, c.TWILIO_AUTH_TOKEN)
     return client.messages.create(
         to=recipient_phone_number, 
         from_=twilio_phone_number,
-        body="Hello! There was update made on the Travel Canada website. Text STOP to opt out."
+        body=body
         )
