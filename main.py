2cac465428b0260a47e21ea97f5343c04080f48c
Bryan Galindo <galindo.bryan08@gmail.com>
2021-06-18 01:57:41 -0500

    Adding business logic file

---
+++ main.py
@@ -0,0 +1,40 @@
+import constants as c
+from database import MongoDB
+from scraper import get_travel_url_html, get_url_last_modified_date, get_exemption_message
+from sms import send_sms_message
+
+
+database = MongoDB(c.MONGO_DB_CONNECTION_STRING, c.DATABASE, c.COLLECTION)
+html = get_travel_url_html(c.TRAVEL_CANADA_URL)
+
+def check_if_collection_empty():
+    if database.get_collection_size() == c.EMPTY:
+        return True
+
+def check_if_site_changed():
+    last_inserted_date = database.get_last_document()['last_modified_date']
+    scraped_date = get_url_last_modified_date(html)
+
+    if scraped_date != last_inserted_date:
+        return True
+
+def insert_scraped_data():
+    _dict = {
+        "id": database.generate_id(),
+        "last_modified_date": get_url_last_modified_date(html),
+        "message": get_exemption_message(html)
+    }
+    return database.insert_one_document(_dict)
+
+
+if __name__ == '__main__':
+    is_empty = check_if_collection_empty()
+    if is_empty:
+        insert_scraped_data()
+    else:
+        is_changed = check_if_site_changed()
+        if is_changed:
+            insert_scraped_data()
+            send_sms_message(c.PERSONAL_PHONE_NUMBER, c.TWILIO_PHONE_NUMBER, c.NOTIFICATION_TEXT)
+    
+        
