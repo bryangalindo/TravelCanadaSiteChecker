cb0441f10f3a4df7ceef9076ebb615f92f1a621f
Bryan Galindo <galindo.bryan08@gmail.com>
2021-06-18 01:03:39 -0500

    Adding database class to store scraped information

---
+++ database.py
@@ -0,0 +1,22 @@
+import pymongo
+import constants as c
+
+
+class MongoDB:
+    def __init__(self, connection_string, database, collection):
+        self.client = pymongo.MongoClient(connection_string)
+        self.db = self.client[database]
+        self.collection = self.db[collection]
+        
+    def get_last_document(self):
+        return self.collection.find().sort("id", -1)[0]
+        
+    def insert_one_document(self, _dict):
+        return self.collection.insert_one(_dict)
+    
+    def generate_id(self):
+        collection_size = self.db.command('collstats', 'dates')['count']
+        if collection_size == c.EMPTY:
+            return c.INITIAL_ID
+        else: 
+            return self.db.command('collstats', 'dates')['count'] + 1
