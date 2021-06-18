9685d3984b0351f9b2dae5f93f3d26c7bcba4bc3
Bryan Galindo <galindo.bryan08@gmail.com>
2021-06-18 01:19:17 -0500

    Refactoring scraper functions to remove logging

--- scraper.py
+++ scraper.py
@@ -1,5 +1,3 @@
-import logging
-
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,20 +5,9 @@ import constants as c
 import helpers as h
 
 
-logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-
 def get_travel_url_html():
-    logger.info('Scraping starting')
     response = requests.get(c.TRAVEL_CANADA_URL)
-    logger.info('Scraping complete')
     if response.status_code == 200:
-        logger.info('Scrape successful')
         return BeautifulSoup(response.content, "html.parser")
 
 def get_exemption_message(html):
