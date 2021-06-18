33a96bd82a7cfb2a6e3552f4648aa2802b5ef209
Bryan Galindo <galindo.bryan08@gmail.com>
2021-06-18 01:57:10 -0500

    Updating notification text to include travel url

--- constants.py
+++ constants.py
@@ -6,7 +6,9 @@ TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 PERSONAL_PHONE_NUMBER = os.getenv("PERSONAL_PHONE_NUMBER")
 
 TRAVEL_CANADA_URL = "https://travel.gc.ca/travel-covid"
-EXEMPTION_TEXT = "Exemptions for fully vaccinated travellers who are eligible to enter Canada are expected in early July. Current travel restrictions still apply."
+NOTIFICATION_TEXT = f"There was a change made on the Travel Canada website. Visit {TRAVEL_CANADA_URL}. Text STOP to opt out."
+DATABASE = "travelcanada"
+COLLECTION = "dates"
 
 MONGO_DB_CONNECTION_STRING = os.getenv("MONGO_DB_CONNECTION_STRING")
 
